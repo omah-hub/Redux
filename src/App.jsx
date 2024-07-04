c821d7b80436ec0d112479acbecaac2177160ed6
omah-hub <orjichioma422@yahoo.com>
2024-07-04 15:43:56 +0100

    my first redux counter

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Counter from "./components/counter";
+const App = () => {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+};
+
+export default App
---
+++ src/components/number.js
@@ -0,0 +1,15 @@
+import { useSelector, useDispatch } from "react-redux"
+import { increment, decrement } from "../app/features/counter/counterSlice"
+const number = () => {
+    const count = useSelector((state) => state.counter.value);
+
+    const dispatch = useDispatch();
+    return (
+    <div>
+        <h1>{count}</h1>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+        
+    </div>
+    );
+}
